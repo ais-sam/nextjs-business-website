8189298289601bd879dbea775461e60275cee2d1
issam <aissam.16.25@gmail.com>
2023-03-13 12:12:04 +0100

    Edit footer

--- components/shared/Footer/index.js
+++ components/shared/Footer/index.js
@@ -13,7 +13,8 @@ const Footer = () => {
           {/* Top */}
           <div className="flex flex-col gap-6 lg:flex-row lg:items-center lg:justify-between">
             <h4 className="text-lg">
-              Have a <Underline text="any questions?" />
+              {/* Have a <Underline text="any questions?" bottom="[100px]" /> */}
+              Have a any <span className='question'>questions</span>
               <br /> Contact Us{" "}
             </h4>
             <div className="flex flex-col  lg:flex-row items-start  lg:items-center gap-4 lg:gap-2">
