3cf5bd5699c30e97e9927f13d80b6ce1dae1b06e
issam <aissam.16.25@gmail.com>
2023-03-21 11:08:28 +0100

    Link Product Card with Product details page

--- components/products/ProductCard.js
+++ components/products/ProductCard.js
@@ -1,13 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 const ProductCard = ({image,title}) => {
   return (
-    <div className='bg-white shadow-sm'>
+    <Link href={'/products/' + title} className='bg-white shadow-sm'>
         <Image src={image}/>
         <div className='p-4'>
             <span>{title}</span>
         </div>
-    </div>
+    </Link>
   )
 }
 
