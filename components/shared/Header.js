573454906ad5e193fbf96287d8f7f9c1682ea9e6
issam <aissam.16.25@gmail.com>
2023-03-27 11:20:03 +0100

    edit the header for mobile devices

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -12,48 +12,79 @@ const Header = () => {
  
   return (
     <div className="max-w-[2000px] mx-auto">
+      {/* Desktop */}
+      <div className="hidden md:block">
+        <div className="py-4 px-2 flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <Image src={phone} width={30} height={30} />
+            <div>
+              <span className="block text-sm">Do you need help?</span>
+              <span>+0(850) 544 7514</span>
+            </div>
+          </div>
 
-      <div className="py-4 px-2 flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <Image src={phone} width={30} height={30}/>
           <div>
-            <span className="block text-sm">Do you need help?</span>
-            <span>+0(850) 544 7514</span>
+            <Logo />
+          </div>
+
+          <div className="flex items-center gap-8">
+            <div className="flex items-center gap-4">
+              <Image src={linkedin} width={18} height={18} alt="" />
+              <Image src={instagram} width={18} height={18} alt="" />
+              <Image src={twitter} width={20} height={20} alt="" />
+            </div>
+            <div className="flex items-center gap-1">
+              <Image src={english} width={32} height={32} alt="" />
+              <div className="font-inter">
+                <span className="block opacity-50 text-xs -mb-1">Language</span>
+                <span className="text-[13px]">English</span>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div>
-          <Logo/>
-        </div>
-
-        <div className="flex items-center gap-8">
-          <div className="flex items-center gap-4">
-              <Image src={linkedin} width={18} height={18} alt=""/>
-              <Image src={instagram} width={18} height={18} alt=""/>
-              <Image src={twitter} width={20} height={20} alt=""/>
-          </div>
-          <div className="flex items-center gap-1">
-          <Image src={english} width={32} height={32} alt=""/>
-          <div className="font-inter">
-            <span className="block opacity-50 text-xs -mb-1">Language</span>
-            <span className="text-[13px]">English</span>
-          </div>
-            
-          </div>
+        <div className="py-2">
+          <nav className="flex items-center justify-center gap-4 capitalize text-[15px]">
+            <Link href="/about">about</Link>
+            <Link href="/services">services</Link>
+            <Link href="/team">team</Link>
+            <Link href="/pricing">pricing</Link>
+            <Link href="/products">products</Link>
+            <Link href="/news">news</Link>
+            <Link href="/contact">contact</Link>
+          </nav>
         </div>
       </div>
 
 
-      <div className="py-2">
-        <nav className="flex items-center justify-center gap-4 capitalize text-[15px]">
-          <Link href="/products">products</Link>
-          <Link href="/">solutions</Link>
-          <Link href="/">softwares</Link>
-          <Link href="/">services</Link>
-          <Link href="/">corporate</Link>
-          <Link href="/news">news</Link>
-          <Link href="/contact">contact</Link>
-        </nav>
+      {/* Mobile */}
+      <div className="md:hidden py-4  px-2">
+        {/* top */}
+        <div className="flex items-center justify-between mb-2">
+          <div>
+            <Logo />
+          </div>
+          <div className="burger-icon">
+            <label className="burger" for="check">
+              <input type="checkbox" id="check" />
+              <span></span>
+              <span></span>
+              <span></span>
+            </label>
+          </div>
+        </div>
+    {/* bottom */}
+        <div className="mobile-menu h-0 overflow-hidden">
+          <nav className="flex flex-col items-start justify-center gap-4 capitalize text-[15px]">
+              <Link href="/about">about</Link>
+              <Link href="/services">services</Link>
+              <Link href="/team">team</Link>
+              <Link href="/pricing">pricing</Link>
+              <Link href="/products">products</Link>
+              <Link href="/news">news</Link>
+              <Link href="/contact">contact</Link>
+            </nav>
+        </div>
       </div>
     </div>
   );
